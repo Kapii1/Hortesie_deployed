9a66f758ef794e9e1d775e75514402d2b783e2b6
Kapii1 <89147630+Kapii1@users.noreply.github.com>
2022-11-28 21:36:28 +0100

    slider

--- hortesie_front/src/components/routes/SimpleSlider.js
+++ hortesie_front/src/components/routes/SimpleSlider.js
@@ -2,26 +2,29 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './SimpleSlider.css'
-export function SimpleSlider(props){
-    
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        swipeToSlide: true,
-      };
-      return(
-        <Slider {...settings} >
-        {props.images.map((item, i) => (
-            <div key={i}>
-                <img  className="image-nested" src={require("./" + item.nom)} key={item.nom} layout="responsive" alt="" />
-            </div>
-        ))}
-      </Slider>
-      )
-
-
+import "./SimpleSlider.css";
+export function SimpleSlider(props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+  };
+  return (
+    <Slider {...settings}>
+      {props.images.map((item, i) => (
+        <div key={i}>
+          <img
+            className="image-nested"
+            src={process.env.PUBLIC_URL + "/" + item.nom}
+            key={item.nom}
+            layout="responsive"
+            alt=""
+          />
+        </div>
+      ))}
+    </Slider>
+  );
 }
