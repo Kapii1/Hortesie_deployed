2a738df517d769d7d0faa0d62dc2927bc5f65c91
Kapii1 <89147630+Kapii1@users.noreply.github.com>
2022-11-28 21:42:19 +0100

    for login

--- hortesie_front/src/components/routes/list-admin/Login.js
+++ hortesie_front/src/components/routes/list-admin/Login.js
@@ -1,54 +1,60 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './Login.css';
-import { Button, TextField } from '@mui/material';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Login.css";
+import { Button, TextField } from "@mui/material";
+import { API_URL } from "../../../url";
 
 async function loginUser(credentials) {
- return fetch('http://localhost:3001/login', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json'
-   },
-   body: JSON.stringify(credentials)
- })
-   .then(data => data.json())
+  return fetch(API_URL + "/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
 }
 
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const token = await loginUser({
       username,
-      password
+      password,
     });
 
     setToken(token);
-  }
+  };
 
-  return(
+  return (
     <div className="login-wrapper">
-      <div className='login-in-wrapper'>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Utilisateur</p>
-          <TextField type="text" onChange={e => setUserName(e.target.value)} />
-        </label>
-        <label>
-          <p>Mot de passe</p>
-          <TextField type="password" onChange={e => setPassword(e.target.value)} />
-        </label>
-        <div className='button-connexion'>
-          <Button type="submit" >Se connecter </Button>
-        </div>
-      </form>
+      <div className="login-in-wrapper">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Utilisateur</p>
+            <TextField
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </label>
+          <label>
+            <p>Mot de passe</p>
+            <TextField
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <div className="button-connexion">
+            <Button type="submit">Se connecter </Button>
+          </div>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
+  setToken: PropTypes.func.isRequired,
 };
