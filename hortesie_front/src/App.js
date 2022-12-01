c4d26f002d67a9771036372e68bf116877bc3c3d
Kapii1 <89147630+Kapii1@users.noreply.github.com>
2022-12-01 22:21:29 +0100

    mobile version

--- hortesie_front/src/App.js
+++ hortesie_front/src/App.js
@@ -15,7 +15,7 @@ function App() {
     console.log(menu_icon);
     navbar.style.left = collapsed ? "0" : "100vw";
     menu_icon.style.transform = collapsed
-      ? "translateX(-90vw)"
+      ? "translateX(-80vw)"
       : "translateX(-10vw)";
   }, [collapsed]);
   return (
