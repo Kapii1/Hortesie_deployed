35c21fbd94571ceafdff9da5491f03f4053d65e0
a_pepin <anthony8.pepin@hotmail.fr>
2023-03-08 23:38:30 +0100

    new slider correct

--- hortesie_front/src/components/routes/Image.js
+++ hortesie_front/src/components/routes/Image.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import 'react-spinner-animated/dist/index.css'
-import { DoubleOrbit } from 'react-spinner-animated';
+import { Spinner } from 'react-spinner-animated';
 import "./Image.css"
 
 export default function Image({ src, className, load, spinner = false }) {
@@ -14,9 +14,9 @@ export default function Image({ src, className, load, spinner = false }) {
         <>
             {!isLoaded || spinner ?
                 <div className="spinner-loading">
-                    <DoubleOrbit text={"Chargement..."} bgColor={"#FFF"} center={false} width={"100px"} height={"100px"} />
+                    <Spinner text={"Chargement..."} bgColor={"#FFF"} center={false} height={"100px"} />
                 </div> : ''}
-            <img src={src} style={{ display: isLoaded ? "" : "none" }} className={className} onLoad={imageLoaded} />
+            {/* <img src={src} style={{ display: isLoaded ? "" : "none" }} className={className} onLoad={imageLoaded} /> */}
         </>
     )
 }
