4b7b3fbef2bb34eb2e99c1f824fa5cee150f24db
a_pepin <anthony8.pepin@hotmail.fr>
2023-03-18 15:07:34 +0100

    correct mobile view

--- hortesie_front/src/components/routes/CustomCarouselItem.js
+++ hortesie_front/src/components/routes/CustomCarouselItem.js
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import Image from "./Image"
-
+import './CustomCarouselItem.css'
 export default function CustomCarouselItem({ src, index, onAllLoad }) {
 
     return (<div>
-        <Image src={src} load={onAllLoad} />
+        <Image src={src} load={onAllLoad} className="carousel-images" />
     </div>
     )
 }
