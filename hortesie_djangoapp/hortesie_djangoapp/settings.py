61edd61bcae71d448e04e85ac51d20827fba5240
a_pepin <anthony8.pepin@hotmail.fr>
2023-08-06 19:18:29 +0200

    settings

--- hortesie_djangoapp/hortesie_djangoapp/settings.py
+++ hortesie_djangoapp/hortesie_djangoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2@ay69s+t9h$v#+4x(-=!)&uwykea+t+ivs$c3kjioc)12jtyo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
